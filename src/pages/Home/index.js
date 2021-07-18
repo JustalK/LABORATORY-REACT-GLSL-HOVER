061a3bbf10fdcce8024e5a08983a0412adcee082
Latsuj <justal.kevin@gmail.com>
2021-07-18 19:21:59 +0800

    [FEATURE] Adding text from troika

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,25 @@
  * The module managing the home page
  * @module Home
  */
-import React, { useRef, useState } from 'react'
-import { useLoader, useFrame } from '@react-three/fiber'
+import React, { useRef, useState, useEffect } from 'react'
+import { useLoader, useFrame, extend } from '@react-three/fiber'
+import { Text } from 'troika-three-text'
 import * as THREE from 'three'
 import Selector from './Selector'
+import * as Selections from './Selections'
+
+const total = Object.keys(Selections).length
+
+extend({ Text })
+
+const propsText = {
+  color: '#99ccff',
+  maxWidth: 300,
+  align: 'center',
+  font: '/font.ttf',
+  anchorX: 'center',
+  anchorY: 'middle'
+}
 
 /**
  * @function Home
@@ -15,6 +30,8 @@ import Selector from './Selector'
  */
 export default function Home() {
   const ref = useRef()
+  const titleRef = useRef()
+  const subtitleRef = useRef()
   const [hover, setHover] = useState(false)
   const [tDiffuse] = useLoader(THREE.TextureLoader, ['./1.jpeg'])
   const [selection, setSelection] = useState(0)
@@ -25,6 +42,11 @@ export default function Home() {
       ? Math.min(1.0, ref.current.uVelo + 0.05)
       : Math.max(0.0, ref.current.uVelo - 0.05)
   })
+
+  useEffect(() => {
+    titleRef.current.sync()
+    subtitleRef.current.sync()
+  }, [])
 
   return (
     <>
@@ -38,6 +60,24 @@ export default function Home() {
         }}
         onClick={() => setSelection((c) => c + 1)}
       >
+        <text
+          position={[0, 0.6, 0]}
+          fontSize={0.08}
+          text={`Experience: ${(selection % total) + 1}`}
+          ref={titleRef}
+          {...propsText}
+        >
+          <meshPhongMaterial />
+        </text>
+        <text
+          position={[0, -0.6, 0]}
+          fontSize={0.06}
+          text={`Click on the image`}
+          ref={subtitleRef}
+          {...propsText}
+        >
+          <meshPhongMaterial />
+        </text>
         <planeGeometry args={[1, 1, 32, 32]} />
         <Selector selection={selection} ref={ref} tDiffuse={tDiffuse} />
       </mesh>
